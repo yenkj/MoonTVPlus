f975da14a9bb5dd80c1a49f9b905b205e34d7326
yenkj <49618620+yenkj@users.noreply.github.com>
2026-02-15 13:38:37 +0800

    Update route.ts

--- src/app/api/admin/config/route.ts
+++ src/app/api/admin/config/route.ts
@@ -31,7 +31,7 @@ export async function GET(request: NextRequest) {
       Role: 'owner',
       Config: config,
     };
-    if (username === process.env.USERNAME || authInfo.role === 'owner') {
+    if (username === process.env.USERNAME) {
       result.Role = 'owner';
     } else {
       // 从新版数据库获取用户信息
