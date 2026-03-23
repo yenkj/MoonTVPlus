e68f44fea86d86183f29e45c26d6149a45c18528
yenkj <49618620+yenkj@users.noreply.github.com>
2026-03-23 14:51:31 +0800

    Update emby.client.ts

--- src/lib/emby.client.ts
+++ src/lib/emby.client.ts
@@ -537,7 +537,7 @@ export class EmbyClient {
       // 带音频参数的 HLS
         const playSessionId = generatePlaySessionId();  
         url = `${this.serverUrl}/Videos/${itemId}/master.m3u8?api_key=${token}&DeviceId=efd03a05-f87b-48ec-9e35-78bf5a1ed1e7&PlaySessionId=${playSessionId}&AudioCodec=mp3,aac`;
-    }
+      }
 
       // 选项2: 拼接MediaSourceId参数
       if (this.appendMediaSourceId) {
